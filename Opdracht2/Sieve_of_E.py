714b7f6e3fa2d0ecb47b80527966dedf5bf7b217
koenstemerdink <koenstemerdink@hotmail.com>
2015-05-04 10:50:04 +0000

    Sneller gemaakt

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,22 +1,26 @@
 import sys
 import time
+import math
 
 N = int(sys.argv[1])
-
+priemgetallen = open(sys.argv[2], 'w')
 T1 = time.perf_counter()
+aantal = 0
 
 lijst = list(range(2,N))
+lijst2 = list(range(3,int(math.sqrt(N)),2))
+lijst2.insert(0,2)
 
-for mini in lijst:
-    for getal in lijst:
-        if (getal%mini==0 and getal!=mini):
-            lijst.remove(getal)
+for getal2 in lijst2:
+    for getal in lijst[(getal2**2)-2:N-1]:
+        if (getal%getal2==0 and getal!=getal2):
+            lijst[getal-2]=0
+
+for getal in lijst:
+    if getal!=0:
+        priemgetallen.write(str(getal) + '\n')
+        aantal += 1
 
 T2 = time.perf_counter()
 
-primes = open(sys.argv[2], 'w')
-
-for getal in lijst:
-    primes.write(str(getal) + '\n')
-
-print('Found', len(lijst), 'Prime numbers smaller than', N, 'in', T2-T1, 'sec.')
+print('Found', str(aantal), 'Prime numbers smaller than', N, 'in', T2-T1, 'sec.')
